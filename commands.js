7650095146382f8a4047ba4712047b61cafbaf94
Jonathan Lowery <business@jonathanmlowery.com>
2025-03-27 01:04:10 -0400

    Basic local command handling

---
+++ commands.js
@@ -0,0 +1,25 @@
+const theme = (args, response) => {
+	document.documentElement.className = `theme-${args[1]}`;
+
+	return null;
+};
+
+const fontsize = (args, response) => {
+	document.documentElement.style.setProperty(
+		"--terminal-font-size",
+		`${args[1]}px`
+	);
+
+	return null;
+};
+
+const help = (args) => {
+	return [
+		"List of commands:",
+		"help -> displays this message",
+		"theme (name) -> changes terminal theme",
+		"fontsize (pixels) -> changes the terminal text size",
+	].join("\n");
+};
+
+export const commands = { theme, fontsize, help };
---
+++ script.js
@@ -0,0 +1,54 @@
+import { commands } from "/commands.js";
+
+const exec_command = (command) => {
+	const output = document.getElementById("terminal-output");
+
+	const prompt_line = document.createElement("p");
+	prompt_line.classList.add("prompt");
+
+	const command_text = document.createElement("span");
+	command_text.classList.add("command");
+	command_text.innerText = command;
+
+	prompt_line.innerText = "user@terminal-website$\u00A0";
+	prompt_line.appendChild(command_text);
+	output.appendChild(prompt_line);
+
+	const args = command.split(" ");
+
+	const response = document.createElement("p");
+	let command_output;
+
+	if (args[0] in commands) {
+		command_output = commands[args[0]](args);
+	}
+
+	console.log(command_output);
+
+	if (command_output) {
+		response.classList.add("output");
+		response.textContent = command_output;
+		output.appendChild(response);
+	}
+};
+
+const command_input = document.getElementById("command-input");
+const command_text = document.getElementById("command-text");
+
+document.addEventListener("click", () => command_input.focus());
+
+command_input.addEventListener("input", () => {
+	const safeText = command_input.value.replace(/ /g, "\u00A0");
+	command_text.innerHTML = safeText;
+});
+
+command_input.addEventListener("keydown", (e) => {
+	if (e.key === "Enter") {
+		e.preventDefault();
+		exec_command(command_input.value);
+		command_input.value = "";
+		command_text.innerHTML = "";
+	}
+});
+
+commands.theme(["theme", "dracula"], {});
