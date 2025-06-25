a8fa3588c36e714b17991af8b3608117fd690800
Michael Soukup <michael@soukup.io>
2025-06-25 13:46:26 +0200

    Refactor and remove site-specific commands and text

--- commands.js
+++ commands.js
@@ -1,22 +1,3 @@
-const welcome_ascii = `<pre>
-         _nnnn_               
-        dGGGGMMb              
-       @p~qp~~qMb             
-       M|@||@) M|             Welcome to <a>jonathanmlowery.com</a>!
-       @,----.JM|             This is an interactive terminal website built with plain HTML, CSS, and JS
-      JS^\\__/  qKL            
-     dZP        qKRb          To view a list of available commands, type '<a>help</a>'
-    dZP          qKKb         
-   fZP            SMMb        Javascript | <a href="https://www.linkedin.com/in/jonathan-lowery-724a7524a/" target="_blank">LinkedIn</a> | Python Institute PCAP
-   HZM            MMMM        Python     | <a href="https://github.com/jonathanmlowery" target="_blank">GitHub</a>   | Comptia A+
-   FqM            MMMM        Java       | <a href="/resume.pdf" target="_blank">Resume</a>   | Comptia Network+
- __| ".        |\\dS"qML       C++        |          |
- |    \`.       | \`' \\Zq       
-_)      \\.___.,|     .'       Email: <a href="mailto:contact@jonathanmlowery.com" target="_blank">contact@jonathanmlowery.com</a>
-\\____   )MMMMMP|   .'        
-     \`-'       \`--'hjm      
-</pre>`;
-
 const theme = (args) => {
 	document.documentElement.className = `theme-${args[1]}`;
 
@@ -36,13 +17,9 @@ const help = (args) => {
 	return (
 		"<pre>" +
 		[
-			"Information:",
 			"  <a>help</a>                 display this message",
-			"  <a>welcome</a>              display welcome message",
-			"  <a>certs</a>                view certifications",
-			"Appearance:",
-			"  <a>theme (name)</a>         change terminal theme (monokai, synthwave, dracula, matrix, solarized)",
-			"  <a>fontsize (pixels)</a>    change the terminal text size",
+			"  <a>theme [NAME]</a>         change terminal theme (monokai, synthwave, dracula, matrix, solarized)",
+			"  <a>fontsize [PIXELS]</a>    change the terminal text size",
 			"  <a>clear</a>                clear previous terminal output",
 			"",
 		].join("\n") +
@@ -56,16 +33,4 @@ const clear = (args) => {
 	return null;
 };
 
-const welcome = (args) => {
-	return welcome_ascii;
-};
-
-const certs = (args) => {
-	return (
-		"<pre>" +
-		["Python Institute PCAP", "Comptia A+", "Comptia Network+"].join("\n") +
-		"</pre>"
-	);
-};
-
-export const commands = { theme, fontsize, help, clear, welcome, certs };
+export const commands = { theme, fontsize, help, clear };
--- terminal.js
+++ terminal.js
@@ -1,8 +1,41 @@
 import { commands } from "/commands.js";
 
+const user = "guest";
+const hostname = window.location.hostname;
 const output = document.getElementById("terminal-output");
 
-const exec_command = (command) => {
+const welcome_ascii = `<pre>
+         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|             Welcome!
+       @,----.JM|             This is an interactive sitemap viewer built with plain HTML, CSS, and JS
+      JS^\\__/  qKL
+     dZP        qKRb          View <a href="https://github.com/msoukup/terminal-sitemap-viewer">this project on GitHub</a>.
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\\dS"qML
+ |    \`.       | \`' \\Zq
+_)      \\.___.,|     .'
+\\____   )MMMMMP|   .'
+     \`-'       \`--'hjm        To view a list of available commands, type '<a>help</a>'
+</pre>`;
+
+
+const set_prompt = (elem) => {
+  elem.innerText = `${user}@${hostname}$\u00A0`;
+};
+
+const print = (content) => {
+  const response = document.createElement("p");
+  response.classList.add("output");
+  response.innerHTML = content.replace(/\n/g, "<br />");
+  output.appendChild(response);
+};
+
+const exec = (command) => {
 	const prompt_line = document.createElement("p");
 	prompt_line.classList.add("prompt");
 
@@ -10,27 +43,22 @@ const exec_command = (command) => {
 	command_text.classList.add("command");
 	command_text.innerText = command;
 
-	prompt_line.innerText = "user@jonathanmlowery.com$\u00A0";
+  set_prompt(prompt_line);
 	prompt_line.appendChild(command_text);
 	output.appendChild(prompt_line);
 
 	const args = command.split(" ");
-
-	const response = document.createElement("p");
 	let command_output;
-
 	if (args[0] in commands) {
+    console.log(args[0]);
 		command_output = commands[args[0]](args);
-	}
+	} else {
+    command_output = args[0] + ": command not found";
+  }
 
-	console.log(command_output);
-
-	if (command_output) {
-		response.classList.add("output");
-		response.innerHTML = command_output.replace(/\n/g, "<br />");
-
-		output.appendChild(response);
-	}
+  if (command_output) {
+    print(command_output);
+  }
 
 	// scrolls to bottom after command is run
 	document.getElementById("command-input").scrollIntoView({ block: "end" });
@@ -52,7 +80,7 @@ let command_index = 1;
 command_input.addEventListener("keydown", (e) => {
 	if (e.key === "Enter") {
 		e.preventDefault();
-		exec_command(command_input.value);
+		exec(command_input.value);
 
 		if (command_input.value) {
 			command_history.push(command_input.value);
@@ -105,14 +133,5 @@ command_input.addEventListener("keydown", (e) => {
 });
 
 commands.theme(["theme", "monokai"]);
-
-const response = document.createElement("p");
-let command_output;
-command_output = commands.welcome(["welcome"]);
-
-if (command_output) {
-	response.classList.add("output");
-	response.innerHTML = command_output.replace(/\n/g, "<br />");
-
-	output.appendChild(response);
-}
+print(welcome_ascii);
+set_prompt(document.getElementById("prompt"));
